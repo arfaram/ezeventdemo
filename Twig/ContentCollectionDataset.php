0124aece3e69b134a48f19eb1335b18d4f9a58d7
Ramzi Arfaoui <ramzi_arfa@hotmail.de>
2019-07-28 20:11:47 +0200

    adding quick content info - add more infos for site administrator

--- Twig/ContentCollectionDataset.php
+++ Twig/ContentCollectionDataset.php
@@ -2,6 +2,10 @@
 
 namespace EzSystems\DemoBundle\Twig;
 
+use ArrayObject;
+use eZ\Publish\API\Repository\LocationService;
+use eZ\Publish\API\Repository\PermissionResolver;
+use EzSystems\EzPlatformAdminUi\Tab\LocationView\DetailsTab;
 use EzSystems\EzPlatformAdminUi\Tab\LocationView\RelationsTab;
 use EzSystems\EzPlatformAdminUi\UI\Dataset\DatasetFactory;
 use Twig\TwigFunction;
@@ -20,13 +24,31 @@ class ContentCollectionDataset extends \Twig_Extension
      * @var \EzSystems\EzPlatformAdminUi\Tab\LocationView\RelationsTab
      */
     private $relationsTab;
+    /**
+     * @var \EzSystems\EzPlatformAdminUi\Tab\LocationView\DetailsTab
+     */
+    private $detailsTab;
+    /**
+     * @var \eZ\Publish\API\Repository\PermissionResolver
+     */
+    private $permissionResolver;
+    /**
+     * @var \eZ\Publish\API\Repository\LocationService
+     */
+    private $locationService;
 
     public function __construct(
         DatasetFactory $datasetFactory,
-        RelationsTab $relationsTab
+        RelationsTab $relationsTab,
+        DetailsTab $detailsTab,
+        PermissionResolver $permissionResolver,
+        LocationService $locationService
     ) {
         $this->datasetFactory = $datasetFactory;
         $this->relationsTab = $relationsTab;
+        $this->detailsTab = $detailsTab;
+        $this->permissionResolver = $permissionResolver;
+        $this->locationService = $locationService;
     }
 
     /**
@@ -41,26 +63,29 @@ class ContentCollectionDataset extends \Twig_Extension
 
     /**
      * @param $content
+     * @param $location
      * @return array
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
      */
-    public function contentCollectionDataSet($content): array
+    public function contentCollectionDataSet($content, $location): array
     {
+        if (false === $this->permissionResolver->hasAccess('setup', 'administrate')) {
+            return [];
+        }
         $contextParameters['content'] = $content;
-        $viewParameters = $this->relationsTab->getTemplateParameters($contextParameters);
+        $contextParameters['location'] = $location;
 
-        $viewParameters['translations'] =  $this->contentTranslations($content->getVersionInfo());
+        $viewParameters = new ArrayObject($this->relationsTab->getTemplateParameters($contextParameters));
 
-        return $viewParameters;
+        $locationChildren = $this->locationService->loadLocationChildren($location);
+
+        $details = $this->detailsTab->getTemplateParameters($contextParameters);
+
+        $details['locationChildrenTotalCount'] = $locationChildren->totalCount;
+
+        return array_merge($viewParameters->getArrayCopy(), $details);
     }
-
-    public function contentTranslations($contentVersionInfo)
-    {
-        $translationsDataset = $this->datasetFactory->translations();
-        $translationsDataset->load($contentVersionInfo);
-
-        return $translationsDataset->getTranslations();
-    }
-
+    
     /**
      * @return string
      */
