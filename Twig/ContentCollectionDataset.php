1dc51bf596d25f94a6bd49f3a1fe46a665f7fa5c
Ramzi Arfaoui <ramzi_arfa@hotmail.de>
2019-11-15 11:24:41 +0100

    Quick Info - Fix for 2.5.6 - relations tab are using pagination up 2.5.6 - Relations and Inverse Relations in quick Info are using a list instead

--- Twig/ContentCollectionDataset.php
+++ Twig/ContentCollectionDataset.php
@@ -3,6 +3,7 @@
 namespace EzSystems\DemoBundle\Twig;
 
 use ArrayObject;
+use eZ\Publish\API\Repository\ContentTypeService;
 use eZ\Publish\API\Repository\LocationService;
 use eZ\Publish\API\Repository\PermissionResolver;
 use EzSystems\EzPlatformAdminUi\Tab\LocationView\DetailsTab;
@@ -36,19 +37,25 @@ class ContentCollectionDataset extends \Twig_Extension
      * @var \eZ\Publish\API\Repository\LocationService
      */
     private $locationService;
+    /**
+     * @var \eZ\Publish\API\Repository\ContentTypeService
+     */
+    private $contentTypeService;
 
     public function __construct(
         DatasetFactory $datasetFactory,
         RelationsTab $relationsTab,
         DetailsTab $detailsTab,
         PermissionResolver $permissionResolver,
-        LocationService $locationService
+        LocationService $locationService,
+        ContentTypeService $contentTypeService
     ) {
         $this->datasetFactory = $datasetFactory;
         $this->relationsTab = $relationsTab;
         $this->detailsTab = $detailsTab;
         $this->permissionResolver = $permissionResolver;
         $this->locationService = $locationService;
+        $this->contentTypeService = $contentTypeService;
     }
 
     /**
@@ -66,6 +73,7 @@ class ContentCollectionDataset extends \Twig_Extension
      * @param $location
      * @return array
      * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
      */
     public function contentCollectionDataSet($content, $location): array
     {
@@ -75,15 +83,13 @@ class ContentCollectionDataset extends \Twig_Extension
         $contextParameters['content'] = $content;
         $contextParameters['location'] = $location;
 
-        $viewParameters = new ArrayObject($this->relationsTab->getTemplateParameters($contextParameters));
-
         $locationChildren = $this->locationService->loadLocationChildren($location);
 
-        $details = $this->detailsTab->getTemplateParameters($contextParameters);
+        $contentDetails = $this->detailsTab->getTemplateParameters($contextParameters);
 
-        $details['locationChildrenTotalCount'] = $locationChildren->totalCount;
+        $contentDetails['locationChildrenTotalCount'] = $locationChildren->totalCount;
 
-        return array_merge($viewParameters->getArrayCopy(), $details);
+        return array_merge($this->getRelationInformation($contextParameters), $contentDetails);
     }
     
     /**
@@ -92,5 +98,48 @@ class ContentCollectionDataset extends \Twig_Extension
     public function getName()
     {
         return 'ez_extension';
+    }
+
+    /**
+     * @param array $contextParameters
+     * @return array
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    public function getRelationInformation(array $contextParameters = []): array
+    {
+        /** @var Content $content */
+        $content = $contextParameters['content'];
+        $relations =
+            $this->datasetFactory
+                ->relationList()
+                ->load($content)
+                ->getRelations();
+
+        foreach ($relations as $relation) {
+            $contentTypeIds[] = $relation->getDestinationContentInfo()->contentTypeId;
+        }
+
+        $contentRelations['relations'] = $relations;
+
+        if (true === $this->permissionResolver->hasAccess('content', 'reverserelatedlist')) {
+            $reverseRelations = $this->datasetFactory
+                ->reverseRelationList()
+                ->load($content)
+                ->getReverseRelations();
+
+            foreach ($reverseRelations as $relation) {
+                $contentTypeIds[] = $relation->getSourceContentInfo()->contentTypeId;
+            }
+
+            $contentRelations['reverse_relations'] = $reverseRelations;
+        }
+
+        if (!empty($contentTypeIds)) {
+            $contentRelations['contentTypes'] = $this->contentTypeService->loadContentTypeList(array_unique($contentTypeIds));
+        } else {
+            $contentRelations['contentTypes'] = [];
+        }
+        return array_replace($contextParameters, $contentRelations);
     }
 }
